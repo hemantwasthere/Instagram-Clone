f710ac604c8cd8398b889afc2173a693929d1783
Hemant <hrsmart2150@gmail.com>
2022-05-02 19:04:18 +0530

    Made Stories Section

--- components/Stories.jsx
+++ components/Stories.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { faker } from '@faker-js/faker';
+import Story from './Story';
 
 function Stories() {
 
-    
+    const [suggestions, setSuggestions] = useState([])
 
-  return (
-    <div>
-        {/* Story */}
-        {/* Story */}
-        {/* Story */}
-        {/* Story */}
-        {/* Story */}
-        {/* Story */}
-        {/* Story */}
-        {/* Story */}
-    </div>
-  )
+    useEffect(() => {
+        const suggestions = [...Array(20)].map((_, i) => ({
+            ...faker.helpers.contextualCard(),
+            id: i,
+        }))
+        console.log(suggestions);
+        setSuggestions(suggestions);
+    }, [])
+
+
+    return (
+        <div className='flex space-x-2 p-6 bg-white mt-8 border-gray-200 rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black'>
+            {suggestions.map(profile => {
+                return <Story key={profile.id} img={profile.avatar} username={profile.username} />
+            })}
+        </div>
+    )
 }
 
 export default Stories
