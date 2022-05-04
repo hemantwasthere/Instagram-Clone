a4ca78ae7bce8dc4115a622314184a0d8ef55dcd
Hemant <hrsmart2150@gmail.com>
2022-05-04 21:39:10 +0530

    Done with comments functionality from firebase

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { BiDotsHorizontalRounded } from 'react-icons/bi'
 import { AiOutlineHeart } from 'react-icons/ai'
 import { HiOutlinePaperAirplane, HiOutlineEmojiHappy } from 'react-icons/hi'
 import { BsChatDots, BsBookmark } from 'react-icons/bs'
 import { useSession } from 'next-auth/react'
+import { addDoc, collection, serverTimestamp, onSnapshot, query, orderBy } from 'firebase/firestore'
+import { db } from '../firebase'
+import Moment from 'react-moment'
 
 
 function Post({ id, username, userImg, img, caption }) {
 
   const { data: session } = useSession();
+  const [comment, setComment] = useState("")
+  const [comments, setComments] = useState([])
+
+  useEffect(() => onSnapshot(
+    query(
+      collection(db, 'posts', id, 'comments'),
+      orderBy('timestamp', 'desc')
+    ),
+    (snapshot) => setComments(snapshot.docs)), [db])
+
+
+  const sendComment = async (e) => {
+    e.preventDefault();
+    const commentToSend = comment;
+    setComment('');
+    await addDoc(collection(db, 'posts', id, 'comments'), {
+      comment: commentToSend,
+      username: session.user.username,
+      userImage: session.user.image,
+      timestamp: serverTimestamp(),
+    })
+  }
 
   return (
     <div className='bg-white my-7 border rounded-sm'>
@@ -43,15 +68,32 @@ function Post({ id, username, userImg, img, caption }) {
       </p>
 
       {/* comments */}
+      {comments.length > 0 && (
+        <div className='ml-10 h-20 overflow-y-scroll scrollbar-thumb-black scrollbar-thin'>
+          {comments.map(comment => (
+            <div key={comment.id} className='flex items-center space-x-2 mb-3'>
+              <img className='rounded-full h-7' src={comment.data().userImage} alt="" />
+              <p className='text-sm flex-1'>
+                <span className='font-bold'>{comment.data().username}</span>
+                {" "}
+                {comment.data().comment}
+              </p>
+              <Moment fromNow className='pr-5 text-xs'>
+                {comment.data().timestamp?.toDate()}
+              </Moment>
+            </div>
+          ))}
+        </div>
+      )}
 
 
       {/* input box */}
       {session && (
         <form className='flex items-center p-4'>
           <HiOutlineEmojiHappy className='h-7' />
-          <input className='border-none flex-1 focus:ring-0 outline-none'
+          <input value={comment} onChange={e => setComment(e.target.value)} className='border-none flex-1 focus:ring-0 outline-none'
             placeholder='Add a comment...' type="text" />
-          <button className='font-semibold text-blue-400'>Post</button>
+          <button type='submit' disabled={!comment.trim()} onClick={sendComment} className='font-semibold text-blue-400'>Post</button>
         </form>
       )}
 
